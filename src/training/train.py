3d712df1cea6a25084887215a528eaceb0e7269f
BughdaryanAram <abughdaryan@biosim.ai>
2024-10-26 18:26:47 +0000

    [Update] minor changes

--- src/training/train.py
+++ src/training/train.py
@@ -13,9 +13,9 @@ if __name__ == "__main__":
     # For the first time, preprocess the data and save it
     # dataset.preprocess_and_save(raw_data_path='raw_mnist', save_path='data/mnist_data', use_zstd=False)
     model = VAE(input_dim=28 * 28, latent_dim=8)
-    trainer = Trainer(model=model, dataset=dataset, batch_size=128, learning_rate=0.001, loss_fn_name='mse')
+    trainer = Trainer(model=model, dataset=dataset, batch_size=128, learning_rate=0.0001, loss_fn_name='mae')
     # trainer._load_snapshot('checkpoints/model_epoch_100.pth')
-    # model.load_state_dict(
-    #     torch.load('/home/abughdaryan/diffusion-journey/checkpoints/model_epoch_140.pth')
-    #     )
+    model.load_state_dict(
+        torch.load('checkpoints/model_epoch_290.pth')
+        )
     trainer.train(epochs=5000)
