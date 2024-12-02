53d86e2c90cd52f2f445a6d0873bb40d1a5806ef
Aram <arambughdaryan@gmail.com>
2024-12-02 08:01:26 +0300

    [Update] add forward pass of VAE_Encoder for diffusion

--- src/diffusion-from-scratch/sd/encoder.py
+++ src/diffusion-from-scratch/sd/encoder.py
@@ -53,12 +53,43 @@ class VAE_Encoder(nn.Sequential):
             # (Batch_size, 512, Height/8, Width/8) -> (Batch_size, 512, Height/8, Width/8)
             VAE_ResidualBlock(512, 512),
 
+            # (Batch_size, 512, Height/8, Width/8) -> (Batch_size, 512, Height/8, Width/8)
             nn.GroupNorm(num_groups=32, num_channels=512),
 
+            # (Batch_size, 512, Height/8, Width/8) -> (Batch_size, 512, Height/8, Width/8)
             nn.SiLU(),
 
+            # (Batch_size, 512, Height/8, Width/8) -> (Batch_size, 8, Height/8, Width/8)
             nn.Conv2d(512, 8, kernel_size=3, padding=1),
 
+            # (Batch_size, 8, Height/8, Width/8) -> (Batch_size, 8, Height/8, Width/8)
             nn.Conv2d(8, 8, stride=3, padding=0)
-            
         )
+    
+    def forward(self, x: torch.Tensor, noise: torch.Tensor) -> torch.Tensor:
+        # x: (Batch_size, Channel, height, width)
+        # noise: (Batch_size, Out_channel, height/8, width/8)
+
+        for module in self:
+            if getattr(module, 'stride', None) == (2, 2):
+                # Padding_Left, Padding_Right, Padding_Top, Padding_Bottom
+                x = F.pad(x, (0, 1, 0, 1))
+            x = module(x)
+        
+        # (Batch_Size, 8, Hight/8, Width/8) -> 2 tensors of shape (Batch_Size, 4, Hight/8, Width/8)
+        mean, log_variance = torch.chunk(x, 2, dim=1)
+
+        # (Batch_Size, 8, Hight/8, Width/8) -> 2 tensors of shape (Batch_Size, 4, Hight/8, Width/8)
+        log_variance = torch.clamp(log_variance, -30, 20)
+        # (Batch_Size, 8, Hight/8, Width/8) -> 2 tensors of shape (Batch_Size, 4, Hight/8, Width/8)
+        variance = log_variance.exp()
+        
+        # (Batch_Size, 8, Hight/8, Width/8) -> 2 tensors of shape (Batch_Size, 4, Hight/8, Width/8)
+        std = torch.sqrt(variance)
+
+        x = mean + std * noise
+
+        # Scale the output by a constant
+        x *= 0.18215
+
+        return x
